a0bc3a02a69da73eee4cc8f4b963d599cd1518bb
Jonathan Bowden <jonathan.bowden@hyperexponential.com>
2024-12-06 17:14:26 +0000

    idk what I'm doing

--- day_06/answer.py
+++ day_06/answer.py
@@ -31,11 +31,30 @@ def check_for_crossings(position, last_3_paths, direction):
         return position[0] > last_3_paths[0][0][0]         
     elif direction == 3: # west
         return position[1] < last_3_paths[0][0][1]
-        
 
 
+def new_obstacle_creates_loop(maze, position, direction, visited_locations_and_dirs, side_locations_and_dirs):
+    
+    # Determine whether if I would to turn here would I end up on the existing path
+    xmax = len(maze)
+    ymax = len(maze[0])
+    x, y = position[0], position[1]
+    direction = (direction+1) % 4
 
+    # search in the new direction for an exact existing path history
+    while True:
+        x, y = move(position, direction)
 
+        if 0 > x or x >= xmax or 0 > y or y >= ymax:
+            return False, side_locations_and_dirs
+        elif maze[x][y] == "#":            
+            direction = (direction+1) % 4
+        elif (x, y, direction) in visited_locations_and_dirs.keys() or (x, y, direction) in side_locations_and_dirs.keys():
+            return True, side_locations_and_dirs
+
+        position = [x,y]
+        side_locations_and_dirs[(x,y,direction)] = 1
+    
 
 def part1(initial_position, maze):
 
@@ -55,51 +74,116 @@ def part1(initial_position, maze):
         position = [x,y]
         visited_locations[(x,y)] = 1
         
-        
+
+
+def new_obstacle_creates_loop_with_silly_exit(maze, position, direction, visited_locations_and_dirs):
+    
+    # Determine whether if I would to turn here would I end up on the existing path
+    xmax = len(maze)
+    ymax = len(maze[0])
+    x, y = position[0], position[1]
+    direction = (direction+1) % 4
+    loop_counter = 0
+
+    # search in the new direction for an exact existing path history
+    while loop_counter < 99999:
+
+        loop_counter += 1
+
+        x, y = move(position, direction)
+
+        if 0 > x or x >= xmax or 0 > y or y >= ymax:
+            return False
+        elif maze[x][y] == "#":            
+            direction = (direction+1) % 4
+        elif (x, y, direction) in visited_locations_and_dirs.keys():
+            return True
+
+        position = [x,y]
+    
+    return True
+
+
 def part2(initial_position, maze):
 
     position = initial_position
     xmax = len(maze)
     ymax = len(maze[0])
-    visited_locations = {}
+    visited_locations_and_dirs = {(initial_position[0], initial_position[1], 0): 1}
+    side_locations_and_dirs = {}
     direction = 0
     last_3_paths = []
     obstacle_count = 0
     last_obstacle = initial_position
     
     while True:
+        
         x, y = move(position, direction)
+        travel_distance = sum(visited_locations_and_dirs.values())+1
+        print(f"I'm here: {position}, going {direction}, I've moved {travel_distance}")
+
         if 0 > x or x >= xmax or 0 > y or y > ymax:
             return obstacle_count
+        
         if maze[x][y] == "#":
-            if len(last_3_paths) == 3:
-                last_3_paths.pop(0)
-            last_3_paths.append((last_obstacle, position))
-            last_obstacle = position
             direction = (direction+1) % 4
+            print(f"OW!, OK I'm turning to {direction}")
             continue
-        # Has the position path crossed the last 3 paths?
-        if len(last_3_paths) == 3:
-            if check_for_crossings(position, last_3_paths, direction):
-                obstacle_count += 1
-    
+
+        # loop, side_locations_and_dirs = new_obstacle_creates_loop(maze, [x,y], direction, visited_locations_and_dirs, side_locations_and_dirs)
+        loop = new_obstacle_creates_loop_with_silly_exit(maze, [x,y], direction, visited_locations_and_dirs)
+        
+        if loop:
+            print(f"You could put an obstacle to stop at {move([x,y],direction)}")
+            obstacle_count += 1
+            
 
         position = [x,y]
-        
-        visited_locations[(x,y)] = 1
-    
-    return obstacle_count
+        visited_locations_and_dirs[(x,y,direction)] = 1
 
 
+
+"""
+Obstacles should be at [6,3], [7,6], [8,1], [8,3], [7, 7], [9,7]
+"""
 
 
 
 if __name__ == '__main__':
     
-    # maze = open_advent_calendar("day_06\day_06_input.txt")    
-    maze = open_advent_calendar("day_06\sample_input_day_06.txt")    
+    maze = open_advent_calendar("day_06\day_06_input.txt")    
+    # maze = open_advent_calendar("day_06\sample_input_day_06.txt")    
     initial_position = [[x, y.find("^")] for x, y in enumerate(maze) if y.find("^") > 0]
     # answer = part1(*initial_position, maze)            
     # print(f"Part 1 answer = {answer}")
     answer = part2(*initial_position, maze)
-    print(f"Part 2 answer = {answer}")
+    print(f"Part 2 answer = {answer}") # 515 too low, 1908 too low, 4988 too high
+
+
+
+"""
+while True:
+x, y = move(position, direction)
+print(f"I'm here: {position}")
+
+if 0 > x or x >= xmax or 0 > y or y > ymax:
+    return obstacle_count
+
+if maze[x][y] == "#":
+    # if len(last_3_paths) == 3:
+    #     last_3_paths.pop(0)
+    # last_3_paths.append((last_obstacle, position))
+    # last_obstacle = position
+    direction = (direction+1) % 4
+    print(f"OW!, OK I'm turning to {direction}")
+    continue
+
+# # Has the position path crossed the last 3 paths?
+# if len(last_3_paths) == 3:
+#     if check_for_crossings(position, last_3_paths, direction):
+#         print(f"You could put an obstacle at {position}")
+#         obstacle_count += 1
+
+position = [x,y]        
+visited_locations[(x,y)] = 1
+    """
